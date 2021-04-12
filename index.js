78d63f0175bb2397a4aa10bc49f278cec73ea249
ParshwaS <parshwa.surat@gmail.com>
2021-04-12 13:54:45 +0530

    First

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -9,7 +10,10 @@ app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 app.use('/api',require('./api'));
-app.use('/', express.static('./public'));
+app.use(express.static('./public'));
+app.get('*', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+})
 
 app.listen(PORT, ()=>{
     console.log("Server is listening on port-"+PORT);
